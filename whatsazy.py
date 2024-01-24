edf6f488621b67cbee7d2189e8ad8b680676f382
WALLYCE <wallyceazy@gmail.com>
2024-01-23 22:28:23 -0300

    Update whatsazy.py

--- whatsazy.py
+++ whatsazy.py
@@ -1,15 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import flet as ft
 global navegador
 
-service = Service(ChromeDriverManager().install())
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-navegador = webdriver.Chrome(service=service, options=options)
+def main(page: ft.Page):
+    page.window_width = 800        # window's width is 200 px
+    page.window_height = 800       # window's height is 200 px
+    page.window_resizable = False  # window is not resizable
 
-navegador.get("https://web.whatsapp.com/")
+    row_1 = ft.Row(alignment=ft.MainAxisAlignment.CENTER)
+    
+
+    row_1.controls.append(
+        ft.Container(
+            height=200,
+            expand= True,
+            alignment=ft.alignment.center,
+            bgcolor=ft.colors.AMBER_200,
+            border=ft.border.all(2, ft.colors.AMBER_600)
+        )
+    )
+    page.add(row_1)
+    page.update()
 
 
-mensatem = """Teste 
- 123"""
+ft.app(target=main)
+
+
+def init():
+    service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    navegador = webdriver.Chrome(service=service, options=options)
+
+    navegador.get("https://web.whatsapp.com/")
+
+
+    mensatem = """Teste 
+    123"""
