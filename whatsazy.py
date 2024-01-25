5a783a85e6bebd00c67ffdea29d9963727adc579
WALLYCE <wallyceazy@gmail.com>
2024-01-24 23:17:25 -0300

    Update whatsazy.py

--- whatsazy.py
+++ whatsazy.py
@@ -2,40 +2,64 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import flet as ft
+
 global navegador
+navegador = None  # Inicialize o navegador como None
 
-def main(page: ft.Page):
-    page.window_width = 800        # window's width is 200 px
-    page.window_height = 800       # window's height is 200 px
-    page.window_resizable = False  # window is not resizable
-
-    row_1 = ft.Row(alignment=ft.MainAxisAlignment.CENTER)
-    
-
-    row_1.controls.append(
-        ft.Container(
-            height=200,
-            expand= True,
-            alignment=ft.alignment.center,
-            bgcolor=ft.colors.AMBER_200,
-            border=ft.border.all(2, ft.colors.AMBER_600)
-        )
-    )
-    page.add(row_1)
-    page.update()
-
-
-ft.app(target=main)
-
-
-def init():
-    service = Service(ChromeDriverManager().install())
+def init(e):
+    global navegador
+    navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    navegador.get("https://web.whatsapp.com/")
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
-    navegador = webdriver.Chrome(service=service, options=options)
-
-    navegador.get("https://web.whatsapp.com/")
-
-
     mensatem = """Teste 
     123"""
+
+def main(page: ft.Page):
+    page.window_width = 800
+    page.window_height = 800
+    page.window_resizable = False
+
+    page.add(
+        ft.ResponsiveRow(
+            [
+                ft.Container(
+                    ft.ResponsiveRow(
+                        [
+                            ft.IconButton(
+                                icon=ft.icons.QR_CODE_SCANNER_ROUNDED,
+                                icon_color="GREEN600",
+                                icon_size=80,
+                                tooltip="Iniciar Whatsapp",
+                                col={"sm": 6, "md": 4, "xl": 2},
+                                on_click=init
+                            ),
+                            ft.IconButton(
+                                icon=ft.icons.PLAY_CIRCLE,
+                                icon_color="BLUE600",
+                                icon_size=80,
+                                tooltip="Iniciar envio",
+                                col={"sm": 6, "md": 4, "xl": 2},
+                            ),
+                            ft.IconButton(
+                                icon=ft.icons.STOP_CIRCLE_OUTLINED,
+                                icon_color="pink600",
+                                icon_size=80,
+                                tooltip="Parar",
+                                col={"sm": 6, "md": 4, "xl": 2},
+                            ),
+                        ],
+                    ),
+                    height=100,
+                    expand=True,
+                    alignment=ft.alignment.center,
+                    bgcolor=ft.colors.LIGHT_GREEN_100,
+                    border=ft.border.all(2, ft.colors.GREY_500),
+                )
+            ]
+        )
+    )
+
+    page.update()
+
+ft.app(target=main)
